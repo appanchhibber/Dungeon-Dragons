13a09634079c0f05c8e46145dc7a916c43193b8e
Appan Chhibber <appanchhibber@hotmail.com>
2017-03-03 08:40:55 -0500

    item assign view changes

--- src/com/SOEN6441_DND/Views/ItemAssignView.java
+++ src/com/SOEN6441_DND/Views/ItemAssignView.java
@@ -5,14 +5,17 @@ import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
-
-import com.SOEN6441_DND.Model.ItemModel.itemTypeList;
-
+/**
+ *This is the view that is responsible for assigning item to the character 
+ * @author Appan Chhibber
+ *
+ */
 public class ItemAssignView extends View {
 public View itemPanel;
 public View backpackPanel;
-public View navPanel;
+public JPanel navPanel;
 
 
 public JLabel itemTypeLabel;
@@ -21,8 +24,13 @@ public JLabel itemSubTypeLabel;
 public JComboBox itemType;
 public JComboBox subItemType;
 
+public JList backpackList;
+public JList assignItemList;
 
 public JButton backpackAssign;
+public JButton charBackButton;
+public JButton addItem;
+public JButton removeItem;
 	@Override
 	protected void initSubviews() {
 		// TODO Auto-generated method stub
@@ -32,7 +40,47 @@ public JButton backpackAssign;
 		itemPanel.setSize(300, 500);
 		itemPanel.setLocation(20,30);
 		itemPanel.setVisible(true);
-
+		
+		backpackPanel=new View();
+		backpackPanel.setBackground(Color.BLACK);
+		backpackPanel.setSize(500, 500);
+		backpackPanel.setLocation(340,30);
+		backpackList=new JList();
+		backpackList.setSize(200, 480);
+		backpackList.setLocation(10, 10);
+		backpackPanel.add(backpackList);
+		
+		addItem=new JButton(">");
+		addItem.setSize(50,30);
+		addItem.setLocation(220, 100);
+		backpackPanel.add(addItem);
+		
+		removeItem=new JButton("<");
+		removeItem.setSize(50,30);
+		removeItem.setLocation(220, 200);
+		backpackPanel.add(removeItem);
+		
+		assignItemList=new JList();
+		assignItemList.setSize(200, 480);
+		assignItemList.setLocation(290, 10);
+		backpackPanel.add(assignItemList);
+		backpackPanel.setVisible(true);
+		
+		
+		navPanel=new JPanel();
+		navPanel.setBackground(Color.BLACK);
+		navPanel.setSize(860, 75);
+		navPanel.setLocation(0,550);
+		navPanel.setVisible(true);
+		backpackAssign=new JButton("Back To  Character Screen");
+		backpackAssign.setSize(240,30);
+		backpackAssign.setLocation(190,45);
+		navPanel.add(backpackAssign);
+		this.setSize(860, 645);
+		this.add(itemPanel);
+		this.add(backpackPanel);
+		this.add(navPanel);
+		this.setVisible(true);
 		itemTypeLabel = new JLabel("Select Item Type:");
 		itemTypeLabel.setSize(100, 20);
 		itemTypeLabel.setLocation(30, 20);
@@ -54,15 +102,21 @@ public JButton backpackAssign;
 		itemPanel.add(subItemType);
 		
 		backpackAssign=new JButton("Add To Backpack");
-		backpackAssign.setSize(120,30);
+		backpackAssign.setSize(160,30);
 		backpackAssign.setLocation(75, 180);
+		
 		itemPanel.add(backpackAssign);
-		backpackPanel=new View();
-		navPanel=new View();
-		this.add(itemPanel);
-		this.add(backpackPanel);
-		this.add(navPanel);
-		this.setVisible(true);
+		
+		
+		
+		
+		
+		
+		
+	
+		charBackButton=new JButton("Back To Character Screen");
+		charBackButton.setSize(240,30);
+		charBackButton.setLocation(190,30);
 		
 	}
 
