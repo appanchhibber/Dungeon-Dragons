5f766e71d70e41151aad1a686e2f374080788a2d
Paras Malik <parasmalik18@gmail.com>
2017-03-03 01:54:51 -0500

    minor changes file operation

--- src/com/SOEN6441_DND/Model/FileOperationModel.java
+++ src/com/SOEN6441_DND/Model/FileOperationModel.java
@@ -1,30 +1,29 @@
 package com.SOEN6441_DND.Model;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.swing.DefaultListModel;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
-
-import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
+
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
+
 import org.dom4j.*;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.SAXReader;
 import org.dom4j.io.XMLWriter;
 import com.SOEN6441_DND.Views.ItemScene;
 
@@ -108,84 +107,82 @@ public class FileOperationModel {
 	}
 
 	/**
+	 * This method is reponsible for saving the item created.
 	 * 
-	 * @param currentScene
+	 * @param currentScene ItemScene
 	 * @author Paras Malik
 	 */
 	public void writeItemData(ItemScene currentScene) {
-
-		DocumentBuilderFactory builderFactory = DocumentBuilderFactory
-				.newInstance();
-		try {
-			DocumentBuilder documentBuilder = builderFactory
-					.newDocumentBuilder();
-
-			//File file = new File("itemSave/"
-				//	+ currentScene.itemType.getSelectedItem().toString()
-					//+ ".xml");
-			if (file.exists()) {
+		
+		File file = new File("itemSave/"+currentScene.itemType.getSelectedItem().toString()+".xml");
+		if(file.exists()){
+		
+			SAXReader reader = new SAXReader();
+			
+				org.dom4j.Document document;
 				try {
-					documentBuilder.parse(file);
-					System.out.println(doc.getElementsByTagName("item"));
-				} catch (SAXException e) {
+					document = reader.read(file);
+					
+					if(document.getRootElement().getName() == currentScene.itemType.getSelectedItem().toString())
+					{
+						
+					
+					org.dom4j.Element rootElement = document.getRootElement();
+					Node helmet=rootElement.selectSingleNode("type");
+					org.dom4j.Element type=rootElement.element("type");
+					List abc=type.elements();
+					System.out.println(abc);
+					
+				
+					
+					
+					}
+				} catch (DocumentException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
+				}
+				
+					
+			
+		}
+		else{
+
+		org.dom4j.Document document = DocumentHelper.createDocument();
+		org.dom4j.Element root = document.addElement(currentScene.itemType.getSelectedItem().toString());
+		
+		org.dom4j.Element typeId = root.addElement("type").addAttribute("Id", "1");
+		typeId.addElement("itemTypeName").addText(currentScene.subItemType.getSelectedItem().toString());
+		
+		typeId.addElement("name").addText(currentScene.nameField.getText());
+		
+		typeId.addElement("enchantValue").addText(currentScene.enchantList.getSelectedItem().toString());
+		
+		
+		try {
+			OutputFormat outputFormat = OutputFormat.createPrettyPrint();
+			FileOutputStream fos = new FileOutputStream(new File("itemSave/"+currentScene.itemType.getSelectedItem().toString()+".xml"));
+			try {
+				XMLWriter xmlWriter = new XMLWriter(fos, outputFormat);
+				try {
+					xmlWriter.write(document);
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-
-			} else {
-				System.out.println("nop exist");
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			// root elements
-			// Document doc = documentBuilder.newDocument();
-			// Element rootElement =
-			// doc.createElement(currentScene.itemType.getSelectedItem().toString());
-			// doc.appendChild(rootElement);
-			//
-			// //item id element
-			// Element itemType = doc.createElement("item");
-			// rootElement.appendChild(itemType);
-			//
-			// //set attribute to item element
-			// Attr attr = doc.createAttribute("id");
-			// attr.setValue("1");
-			// itemType.setAttributeNode(attr);
-			//
-			// //item elements
-			// Element itemSubType = doc.createElement("itemtype");
-			// itemSubType.appendChild(doc.createTextNode(currentScene.subItemType.getSelectedItem().toString()));
-			// itemType.appendChild(itemSubType);
-			//
-			// //second element
-			// Element name = doc.createElement("name");
-			// name.appendChild(doc.createTextNode(currentScene.nameField.getText()));
-			// itemType.appendChild(name);
-			//
-			// //third element
-			// Element enchantValue = doc.createElement("enchantValue");
-			// enchantValue.appendChild(doc.createTextNode(currentScene.enchantList.getSelectedItem().toString()));
-			// itemType.appendChild(enchantValue);
-			//
-			TransformerFactory transformerFactory = TransformerFactory
-					.newInstance();
-			Transformer transformer = transformerFactory.newTransformer();
-			transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-			DOMSource source = new DOMSource(doc);
-			StreamResult streamResult = new StreamResult(new File("itemSave/"
-					+ currentScene.itemType.getSelectedItem().toString()
-					+ ".xml"));
-
-			transformer.transform(source, streamResult);
-			System.out.println("File Saved!!");
-
-		} catch (ParserConfigurationException | TransformerException e) {
+		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
+		  
+		}
+		
+		
+		
 	}
 
 	/**
